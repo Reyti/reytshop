636c5113df507528094c928ec9b408e49c0afb78
Dmytro Fitsych <dimaficych@gmail.com>
2023-09-19 14:11:40 +0200

    Structure

---
+++ pages/products.js
@@ -0,0 +1,29 @@
+import Header from "@/components/header";
+import styled from "styled-components";
+import Center from "@/components/Center";
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Product";
+import ProductsGrid from "@/components/ProductGrid";
+import Title from "@/components/Title";
+
+export default function ProductsPage({ products }) {
+  return (
+    <>
+      <Header />
+      <Center>
+        <Title>All products</Title>
+        <ProductsGrid products={products} />
+      </Center>
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  await mongooseConnect();
+  const products = await Product.find({}, null, { sort: { _id: -1 } });
+  return {
+    props: {
+      products: JSON.parse(JSON.stringify(products)),
+    },
+  };
+}
