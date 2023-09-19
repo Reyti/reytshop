6ccfa208e2a1ad8e6ad394835c79ab1187b88660
Dmytro Fitsych <dimaficych@gmail.com>
2023-09-19 15:08:12 +0200

    Changed function to upper case

--- pages/api/cart.js
+++ pages/api/cart.js
@@ -1,7 +1,7 @@
 import { Product } from "@/models/Product";
 import { mongooseConnect } from "@/lib/mongoose";
 
-export default async function handle(req, res) {
+export default async function Handle(req, res) {
   await mongooseConnect();
   const ids = req.body.ids;
   const products = await Product.find({ _id: ids });
