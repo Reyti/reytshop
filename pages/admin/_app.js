3bf5db1bc7e14d08ff4962ee488ba09568bf0049
Dmytro Fitsych <dimaficych@gmail.com>
2023-09-19 15:09:59 +0200

    Created _app.js for admin

---
+++ pages/admin/_app.js
@@ -0,0 +1,14 @@
+import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+import React from "react";
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+}
